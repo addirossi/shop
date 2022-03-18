2c9627ff5bfe2ce8e5be7abb8097d8f02acd905d
addirossi <addirossi@gmail.com>
2022-03-18 15:48:53 +0600

    Add project

---
+++ main/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Category, Product
+
+
+def index_page(request):
+    categories = Category.objects.all()
+    return render(request, 'main/index.html', {'categories': categories})
+
+
+
+
+#TODO: сделать список продуктов
+#TODO: авторизация
+#TODO: фильтрация, поиск, пагинация
+#TODO: корзина
+#TODO: заказы
+#TODO: отправка писем
+#TODO: деплой
+#TODO: верстка
+
---
+++ order/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+# class Order(models.Model):
+#     user = models.ForeignKey()
+#     products = models.ManyToManyField()
+#     created_at = models.DateTimeField(auto_now_add=True)
